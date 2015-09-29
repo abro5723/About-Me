6599f7561333c0431dfa0a177b7b6435afc52f3b
abro5723 <abro5723@740s-pl-114.local>
2015-09-29 09:51:29 -0600

    Changes in declarations, and variables

--- app/src/main/java/com/example/abro5723/aboutme/MainActivity.java
+++ app/src/main/java/com/example/abro5723/aboutme/MainActivity.java
@@ -4,13 +4,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button eatingButton;
+    private Button sleepingButton;
+    private Button gamingButton;
+    private Button programmingButton;
+    private Button accentButton;
+    private ImageView accentsView;
+    private ImageView gamingView;
+    private ImageView sleepgView;
+    private ImageView eatingView;
+    private ImageView programmingView;
+    private TextView eatingText;
+    private TextView sleepingText;
+    private TextView gamingText;
+    private TextView prorammingText;
+    private TextView accentsText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eatingButton = (Button) findViewById(R.id.eatingButton);
+        sleepingButton = (Button) findViewById(R.id.sleepingButton);
+        gamingButton = (Button) findViewById(R.id.gamingButton);
     }
 
     @Override
