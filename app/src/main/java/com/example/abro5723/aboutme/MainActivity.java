d484c24516e8fc88884e898e2217ce050579d854
abro5723 <abro5723@740s-pl-114.local>
2015-10-08 08:45:21 -0600

    Finished app

--- app/src/main/java/com/example/abro5723/aboutme/MainActivity.java
+++ app/src/main/java/com/example/abro5723/aboutme/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.abro5723.aboutme;
 
+import android.support.v4.app.NotificationCompatSideChannelService;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,13 +20,13 @@ public class MainActivity extends AppCompatActivity {
     private Button accentButton;
     private ImageView accentsView;
     private ImageView gamingView;
-    private ImageView sleepgView;
+    private ImageView sleepingView;
     private ImageView eatingView;
     private ImageView programmingView;
     private TextView eatingText;
     private TextView sleepingText;
     private TextView gamingText;
-    private TextView prorammingText;
+    private TextView programmingText;
     private TextView accentsText;
 
 
@@ -37,6 +38,21 @@ public class MainActivity extends AppCompatActivity {
         eatingButton = (Button) findViewById(R.id.eatingButton);
         sleepingButton = (Button) findViewById(R.id.sleepingButton);
         gamingButton = (Button) findViewById(R.id.gamingButton);
+        programmingButton = (Button) findViewById(R.id.programmingButton);
+        accentButton = (Button) findViewById(R.id.accentButton);
+        accentsView = (ImageView) findViewById(R.id.accentsView);
+        gamingView = (ImageView) findViewById(R.id.gamingView);
+        sleepingView = (ImageView) findViewById(R.id.sleepView);
+        eatingView = (ImageView) findViewById(R.id.eatingView);
+        programmingView = (ImageView) findViewById(R.id.programmingView);
+        eatingText = (TextView) findViewById(R.id.eatingText);
+        sleepingText = (TextView) findViewById(R.id.sleepingText);
+        gamingText = (TextView) findViewById(R.id.gamingText);
+        programmingText = (TextView) findViewById(R.id.programmingText);
+        accentsText = (TextView) findViewById(R.id.accentText);
+
+        setupListeners();
+
     }
 
     @Override
@@ -59,5 +75,161 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void changeAccentVisibility()
+    {
+        if(accentsText.getVisibility() == View.GONE && accentsView.getVisibility() == View.GONE)
+        {
+            accentsText.setVisibility(View.VISIBLE);
+            accentsView.setVisibility(View.VISIBLE);
+            programmingText.setVisibility(View.GONE);
+            programmingView.setVisibility(View.GONE);
+            eatingView.setVisibility(View.GONE);
+            eatingText.setVisibility(View.GONE);
+            sleepingView.setVisibility(View.GONE);
+            sleepingText.setVisibility(View.GONE);
+            gamingView.setVisibility(View.GONE);
+            gamingText.setVisibility(View.GONE);
+        }
+        else
+        {
+            accentsText.setVisibility(View.GONE);
+            accentsView.setVisibility(View.GONE);
+        }
+    }
+
+    private void changeProgramVisibility()
+    {
+        if(programmingText.getVisibility() == View.GONE && programmingView.getVisibility() == View.GONE)
+        {
+            programmingText.setVisibility(View.VISIBLE);
+            programmingView.setVisibility(View.VISIBLE);
+            eatingView.setVisibility(View.GONE);
+            eatingText.setVisibility(View.GONE);
+            sleepingView.setVisibility(View.GONE);
+            sleepingText.setVisibility(View.GONE);
+            gamingView.setVisibility(View.GONE);
+            gamingText.setVisibility(View.GONE);
+            accentsText.setVisibility(View.GONE);
+            accentsView.setVisibility(View.GONE);
+        }
+        else
+        {
+            programmingText.setVisibility(View.GONE);
+            programmingView.setVisibility(View.GONE);
+        }
+    }
+
+    private void changeGameVisibility()
+    {
+        if(gamingText.getVisibility() == View.GONE && gamingView.getVisibility() == View.GONE)
+        {
+            gamingView.setVisibility(View.VISIBLE);
+            gamingText.setVisibility(View.VISIBLE);
+            eatingView.setVisibility(View.GONE);
+            eatingText.setVisibility(View.GONE);
+            sleepingView.setVisibility(View.GONE);
+            sleepingText.setVisibility(View.GONE);
+            accentsText.setVisibility(View.GONE);
+            accentsView.setVisibility(View.GONE);
+            programmingText.setVisibility(View.GONE);
+            programmingView.setVisibility(View.GONE);
+
+        }
+        else
+        {
+            gamingView.setVisibility(View.GONE);
+            gamingText.setVisibility(View.GONE);
+        }
+    }
+
+    private void changeSleepVisibility()
+    {
+        if(sleepingText.getVisibility() == View.GONE && sleepingView.getVisibility() == View.GONE)
+        {
+            sleepingView.setVisibility(View.VISIBLE);
+            sleepingText.setVisibility(View.VISIBLE);
+            eatingView.setVisibility(View.GONE);
+            eatingText.setVisibility(View.GONE);
+            programmingText.setVisibility(View.GONE);
+            programmingView.setVisibility(View.GONE);
+            gamingText.setVisibility(View.GONE);
+            gamingView.setVisibility(View.GONE);
+            accentsText.setVisibility(View.GONE);
+            accentsView.setVisibility(View.GONE);
+        }
+        else
+        {
+            sleepingText.setVisibility(View.GONE);
+            sleepingView.setVisibility(View.GONE);
+        }
+    }
+
+    private void changeEatingVisibility()
+    {
+        if(eatingText.getVisibility() == View.GONE && eatingView.getVisibility() == View.GONE)
+        {
+            eatingText.setVisibility(View.VISIBLE);
+            eatingView.setVisibility(View.VISIBLE);
+            programmingText.setVisibility(View.GONE);
+            programmingView.setVisibility(View.GONE);
+            gamingText.setVisibility(View.GONE);
+            gamingView.setVisibility(View.GONE);
+            accentsText.setVisibility(View.GONE);
+            accentsView.setVisibility(View.GONE);
+            sleepingView.setVisibility(View.GONE);
+            sleepingText.setVisibility(View.GONE);
+        }
+        else
+        {
+            eatingText.setVisibility(View.GONE);
+            eatingView.setVisibility(View.GONE);
+        }
+    }
+
+    private void setupListeners()
+    {
+        eatingButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View buttonView)
+            {
+                changeEatingVisibility();
+            }
+        });
+
+        sleepingButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View buttonView)
+            {
+                changeSleepVisibility();
+            }
+        });
+
+        gamingButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View buttonView)
+            {
+                changeGameVisibility();
+            }
+        });
+
+        programmingButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View buttonView)
+            {
+                changeProgramVisibility();
+            }
+        });
+
+        accentButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View buttonView)
+            {
+                changeAccentVisibility();
+            }
+
+        });
+
     }
 }
